510a7ba961c87fe36306046ce3abf4ffbd52b904
Jérôme Prudent <jprudent@gmail.com>
2018-09-06 00:12:10 +0200

    wip

--- src/ygo/core.cljs
+++ src/ygo/core.cljs
@@ -12,20 +12,24 @@
 (defn find-by-id [id]
   (first (filter #(= id (:Id %)) cards/cards)))
 
-(defn fusion [{id1 :Id fusions :Fusions :as card1}
-              {id2 :Id :as card2}]
+(defn fusion* 
+  [{id1 :Id fusions :Fusions :as card1}
+  {id2 :Id :as card2}]
   (->> fusions
-       (filter (fn [{:keys [_card1 _card2]}]
-                 (and (= _card1 id1) (= _card2 id2))))
-       (map (fn [{:keys [_result]} ] (find-by-id _result)))
-       (first)))  
+    (filter (fn [{:keys [_card1 _card2]}]
+              (and (= _card1 id1) (= _card2 id2))))
+    (map (fn [{:keys [_result]} ] (find-by-id _result)))
+    (first)))
+
+(defn fusion [card1 card2]
+  (or (fusion* card1 card2) (fusion* card2 card1)))  
 
 (defn shallow-fusions [cards]
   (for [i (range (count cards))
         :let [a (first (drop i cards))]
         j (range (inc i) (count cards))
         :let [b (first (drop j cards))
-              fusioned (or (fusion a b) (fusion b a))]
+              fusioned (fusion a b)]
         :when fusioned]
     [a b fusioned]))
 
@@ -41,4 +45,17 @@
                             (remove-one b cards)
                             (conj cards fusioned))]
         (conj (deep-fusions new-cards) fusioned-ab)))
-  (shallow-fusions cards)))
+    (shallow-fusions cards)))
+
+(defn all-fusions [hand board]
+  (reduce
+    (fn [fusions [_ _ hand-card :as hand-fusion]]
+      (concat fusions 
+              [hand-fusion]
+              (for [board-card board
+                    :let [board-fusion (fusion board-card hand-card)]
+                    :when board-fusion]
+                board-fusion)))
+    []
+    (deep-fusions hand)))
+
